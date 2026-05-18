fa860786ade47f727e85fd83b238135d19833098
wlasitz-jeremy <wlasitz@gmail.com>
2026-05-17 20:56:45 -0600

    added multiple header files

--- C/main.c
+++ C/main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <ctype.h>
+#include <limits.h>
+#include <float.h>
+#include <time.h>
+
 
 int main() {
     int age;
@@ -10,7 +18,7 @@ int main() {
     char month[] = "May";
     int year = 2001;
     
-    
+
     printf("My name is %s and I am %d years old as I  born on %s %d, %d.\n", name, age, month, day, year);
     
     return 0;
