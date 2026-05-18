1d9ab497de46d6277304ba75a7a8240e605ce01e
wlasitz-jeremy <wlasitz@gmail.com>
2026-05-18 10:38:26 -0600

    finished digital clock program in c language

--- C/main.c
+++ C/main.c
@@ -27,17 +27,34 @@
 #include <wctype.h>
 #include <unistd.h>
 
-int main() {
-    int age;
-    printf("Enter your age: ");
-    scanf("%d", &age);
-    char name[] = "Celine Gilbert";
-    int day = 7;
-    char month[] = "May";
-    int year = 2001;
+// int main() {
+//     int age;
+//     printf("Enter your age: ");
+//     scanf("%d", &age);
+//     char name[] = "Celine Gilbert";
+//     int day = 7;
+//     char month[] = "May";
+//     int year = 2001;
     
 
-    printf("My name is %s and I am %d years old as I  born on %s %d, %d.\n", name, age, month, day, year);
+//     printf("My name is %s and I am %d years old as I  born on %s %d, %d.\n", name, age, month, day, year);
     
+//     return 0;
+// }
+
+int main(){
+    //This program is a working digital clock that displays the current time in hours, minutes, and seconds.
+    time_t rawtime =0;
+    struct tm *ptime = NULL;
+    bool isRunning = true;
+    printf("DIGITAL CLOCK\n");
+    while (isRunning)
+    {
+        time(&rawtime);
+        ptime = localtime(&rawtime);
+        printf("\r%02d:%02d:%02d", ptime->tm_hour, ptime->tm_min, ptime->tm_sec);
+        fflush(stdout);
+        sleep(1);
+    }
     return 0;
-}
+};
