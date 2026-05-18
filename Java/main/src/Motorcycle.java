448600c816fb18c7cc1b44bb0fb66ca2fc31a92f
wlasitz-jeremy <wlasitz@gmail.com>
2026-05-18 12:45:15 -0600

    added java files to practice OOP-2

---
+++ Java/main/src/Motorcycle.java
@@ -0,0 +1,7 @@
+public class Motorcycle {
+    public static void testMain(String[] args) {
+
+        Motorcycle projectBike = new Motorcycle();
+        System.out.println(Motorcycle.name);
+    }
+}
